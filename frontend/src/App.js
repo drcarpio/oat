f0b334a7cc300390ee12cdb65051978445bd87e9
Jose Carpio <josecarpio@Joses-MBP.lan>
2020-07-31 02:02:49 -0500

    divided app using react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
 import BowlBuilder from './components/BowlBuilder'
@@ -9,11 +10,23 @@ import loginService from './services/login'
 import bowlService from './services/bowls'
 import ingredientService from './services/ingredients'
 
+const Home = ({ notification, isGoodNotification }) => {
+    return (
+        <div>
+            <Notification message={notification} isGood={isGoodNotification} />
+            <h2>Oat app</h2>
+        </div>
+    )
+}
 const App = () => {
     const [notification, setNotification] = useState(null)
     const [isGoodNotification, setIsGoodNotification] = useState(false)
     const [user, setUser] = useState(null)
     const [ingredients, setIngredients] = useState([])
+
+    const padding = {
+        padding: 5,
+    }
 
     useEffect(() => {
         ingredientService
@@ -38,16 +51,56 @@ const App = () => {
     }
 
     return (
-        <div>
-            <Notification message={notification} isGood={isGoodNotification} />
-            {user ? null : <LoginForm loginEvent={handleLogin} />}
-            <BowlBuilder
-                setNotification={setNotification}
-                setIsGoodNotification={setIsGoodNotification}
-                ingredients={ingredients}
-            />
-            <BowlList ingredients={ingredients} user={user} />
-        </div>
+        <Router>
+            <div>
+                <Link style={padding} to="/">
+                    home
+                </Link>
+                <Link style={padding} to="/build">
+                    build
+                </Link>
+                <Link style={padding} to="/menu">
+                    menu
+                </Link>
+                <Link style={padding} to="/profile">
+                    profile
+                </Link>
+                <Link style={padding} to="/basket">
+                    basket
+                </Link>
+            </div>
+
+            <Switch>
+                <Route path="/build">
+                    <BowlBuilder
+                        setNotification={setNotification}
+                        setIsGoodNotification={setIsGoodNotification}
+                        ingredients={ingredients}
+                    />
+                </Route>
+                <Route path="/menu">
+                    <BowlList ingredients={ingredients} user={user} />
+                </Route>
+                <Route path="/profile">
+                    {user ? (
+                        `${user.name} logged in`
+                    ) : (
+                        <LoginForm loginEvent={handleLogin} />
+                    )}
+                </Route>
+                <Route path="/basket">
+                    <div>
+                        <h2>your order</h2>
+                    </div>
+                </Route>
+                <Route path="/">
+                    <Home
+                        notification={notification}
+                        isGoodNotification={isGoodNotification}
+                    />
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
