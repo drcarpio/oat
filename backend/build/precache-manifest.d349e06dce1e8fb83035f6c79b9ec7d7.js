83cf7a9e0012c8a81b7c36511b41a29232b870c0
Jose Carpio <jac2391@columbia.edu>
2020-11-16 00:59:40 -0600

    build

---
+++ backend/build/precache-manifest.d349e06dce1e8fb83035f6c79b9ec7d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fca3d7333dcba9a3af77ec397d853db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb5b4dc8930b1a55f94e",
+    "url": "/static/css/main.dc16932a.chunk.css"
+  },
+  {
+    "revision": "f325e0bd348f0159a5b5",
+    "url": "/static/js/2.b9698cb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b9698cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5b4dc8930b1a55f94e",
+    "url": "/static/js/main.c0001f6a.chunk.js"
+  },
+  {
+    "revision": "82d548ec5cb5730965cd",
+    "url": "/static/js/runtime-main.826d762e.js"
+  }
+]);
